945f1e0f583a366b19978ff06036da0b51a93157
陈智灵 <zhiling.chen@davyin.com>
2022-03-04 16:31:35 +0800

    udpate

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 const path = require('path');
 const ProgressBar = require('progress');
 const { exec } = require('child_process');
-// const roomUrls = require('../mame-room-urls.json');
+const roomUrls = require('../mame-room-urls.json');
 
-let roomUrls = [
-  "https://archive.org/download/mame-merged/mame-merged/005.zip",
-  "https://archive.org/download/mame-merged/mame-merged/100lions.zip",
-  "https://archive.org/download/mame-merged/mame-merged/10yard.zip",
-  "https://archive.org/download/mame-merged/mame-merged/110dance.zip",
-  "https://archive.org/download/mame-merged/mame-merged/11beat.zip",
-  "https://archive.org/download/mame-merged/mame-merged/15lions.zip",
-  "https://archive.org/download/mame-merged/mame-merged/18w.zip",
-  "https://archive.org/download/mame-merged/mame-merged/18wheelr.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1941.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1942.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1943.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1943kai.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1943mii.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1944.zip",
-  "https://archive.org/download/mame-merged/mame-merged/1945kiii.zip",
-]
 let timer = null;
 let step = 50;
 let index = 0;
